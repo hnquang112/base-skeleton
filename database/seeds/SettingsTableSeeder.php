e9299301467caee5cb04b3dbf5479386e58e6abe
Ho Nhat Quang <hnquang112@yahoo.com.vn>
2016-08-16 00:02:33 +0700

    add setting seeder

---
+++ database/seeds/SettingsTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Setting;
+
+class SettingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Set default language of front page is Vietnamese
+        Setting::updateOrCreate(['meta->label' => 'front_page_language'], [
+            'label' => 'front_page_language',
+            'type' => Setting::TYP_CONFIG,
+            'value' => 'vi'
+        ]);
+
+        // Set default language of cms page is English
+        Setting::updateOrCreate(['meta->label' => 'cms_page_language'], [
+            'label' => 'cms_page_language',
+            'type' => Setting::TYP_CONFIG,
+            'value' => 'en'
+        ]);
+
+        // Seed the slider
+        $faker = Faker\Factory::create();
+
+        $limit = 10;
+
+        for ($i = 0; $i < $limit; $i++) {
+            $slider = new Setting;
+            $slider->type = Setting::TYP_SLIDER;
+            $slider->image_id = create_file_from_path($faker->imageUrl(960, 416, 'cats'));
+            $slider->label = $faker->catchPhrase;
+
+            $slider->save();
+        }
+    }
+}
