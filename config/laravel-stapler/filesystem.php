06dfafb7418b5c0553f527d479c7b5e5f0349401
Quang Ho <quang.ho@dwork.vn>
2016-08-29 09:25:21 +0700

    chmod laravel-stapler

--- config/laravel-stapler/filesystem.php
+++ config/laravel-stapler/filesystem.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'url' => '/uploads/:id_partition/:style/:filename',
+    'url' => '/system/:class/:attachment/:id_partition/:style/:filename',
 
     /*
     |--------------------------------------------------------------------------
